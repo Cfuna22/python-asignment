2ff6f11eb5fe23f60cdc571cc472903e410bda0d
Cfuna22 <abelsifuna515@gmail.com>
2025-04-18 00:24:28 +0300

    I love python

---
+++ main.py
@@ -0,0 +1,16 @@
+from pet import Pet
+
+# Create a pet object
+my_pet = Pet("Shadow")
+
+# Test its behavior
+my_pet.get_status()
+my_pet.eat()
+my_pet.sleep()
+my_pet.play()
+my_pet.get_status()
+
+# Bonus methods
+my_pet.train("roll over")
+my_pet.train("play dead")
+my_pet.show_tricks()
